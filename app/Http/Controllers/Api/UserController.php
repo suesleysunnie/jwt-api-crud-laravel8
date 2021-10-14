5e36aa918eaf790303fe2f9d3a3610725a9e023e
Suesley Sunnie <suesleysunnie@hotmail.com>
2021-10-14 11:21:34 -0300

    First commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->password = Hash::make($request->input('password'));
+        $user->email = $request->input('email');
+
+        if( $user->save() ){
+            return response()->json($user);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        $user = User::findOrFail($user->id);
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user = User::findOrFail($user->id);
+        $user->name = $request->input('name');
+        $user->password = Hash::make($request->input('password'));
+        
+        if( $user->save() ){
+            return response()->json($user);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user = User::findOrFail($user->id);
+        
+        if( $user->delete()){
+            return response()->json('User destroyed!');
+        }
+    }
+}
